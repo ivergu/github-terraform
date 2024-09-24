90b563a8fcd5b1b54bef48965da76d580736eb7d
Iver <ivertg@stud.ntnu.no>
2024-09-24 14:50:18 +0200

    SA LRS

---
+++ backend/variables.tf
@@ -0,0 +1,29 @@
+variable "rg_backend_name" {
+    type = string
+    description = "(optional) describe your variable"
+}
+
+variable "rg_backend_location" {
+    type = string
+    description = "(optional) describe your variable"
+}
+
+variable "sa_backend_name" {
+    type = string
+    description = "(optional) describe your variable"
+}
+
+variable "sc_backend_name" {
+    type = string
+    description = "(optional) describe your variable"
+}
+
+variable "kv_backend_name" {
+    type = string
+    description = "(optional) describe your variable"
+}
+
+variable "sa_backend_accesskey_name" {
+    type = string
+    description = "(optional) describe your variable"
+}
