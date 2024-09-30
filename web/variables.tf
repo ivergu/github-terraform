07d5ed86e984c78d9f3f2c1d5c564b83d5082b76
Iver <ivertg@stud.ntnu.no>
2024-09-30 14:08:29 +0200

    t

--- web/variables.tf
+++ web/variables.tf
@@ -1,30 +1,31 @@
 variable "rg_name" {
     type = string
     description = "(optional) describe your variable"
-    
+    default = "rg-website"
 }
 
 variable "rg_location" {
     type = string
     description = "(optional) describe your variable"
+    default = "norwayeast"
 
 }
 
 variable "sa_name" {
     type = string
     description = "(optional) describe your variable"
-    
+    default = "saweb" 
 }
 
 variable "source_content" {
     type = string
     description = "(optional) describe your variable"
-    
+    default = "<h1>Web page made with terraform</h1>"
 }
 
 variable "index_document" {
     type = string
     description = "(optional) describe your variable"
-    
+    default = "index.html"
 }
 
